06245a043f5ec116027bf0d4962c0a348a1f79ae
mercibac <60368815+mercibac@users.noreply.github.com>
2023-07-30 22:25:47 +0000

    trouble.nvim plugin added

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -139,6 +139,19 @@ local plugins = {
       vim.fn["mkdp#util#install"]()
     end,
   },
+
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    },
+    keys = {
+      { "<leader>i", "<cmd>Trouble<cr>", desc = "touble" },
+    },
+  },
   -- UFO folding
   -- {
   --   "kevinhwang91/nvim-ufo",
