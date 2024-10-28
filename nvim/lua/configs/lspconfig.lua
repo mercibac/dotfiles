650eb4d5095f111f7d9032063dd62bc00ebd8f17
mercibac <mercibacishoge@gmail.com>
2024-10-28 22:12:57 +0100

    update tsserver to ts_ls

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -6,28 +6,20 @@ local on_init = configs.on_init
 local capabilities = configs.capabilities
 
 local lspconfig = require "lspconfig"
-local servers = {"pyright","tailwindcss", "html", "lua_ls", "cssls"}
+local servers = {"pyright","tailwindcss", "html", "lua_ls", "cssls", "tsserver"}
 
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_init = on_init, 
+    if lsp == "tsserver" then
+      lsp = "ts_ls"
+    end
+    lspconfig[lsp].setup {
+    on_init = on_init,
     on_attach = on_attach,
     capabilities = capabilities,
   }
 end
 
 lspconfig.svelte.setup { filetypes = { "svelte" } }
-
-lspconfig.tsserver.setup{
-  on_attach = on_attach,
-  capabilities = capabilities,
-  init_options = {
-    preferences = {
-      disableSuggestions = true,
-    }
-  }
-}
-
 
 lspconfig.asm_lsp.setup{
   on_attach = on_attach,
