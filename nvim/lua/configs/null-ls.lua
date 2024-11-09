94226e5cd1a0b6e4caa102dd42f4289988171641
mercibac <mercibacishoge@gmail.com>
2024-11-09 09:24:28 +0200

    update ESLint configuration

--- nvim/lua/configs/null-ls.lua
+++ nvim/lua/configs/null-ls.lua
@@ -4,12 +4,26 @@ local null_ls = require('null-ls')
 local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
 
+
 local sources = {
 
   formatting.black.with { filetypes = { "python" } },
   formatting.prettier,
   lint.ruff.with { filetypes = { "python" }, extra_args = { "--max-line-length=120" } },
-  lint.eslint,
+  -- lint.eslint,
+  lint.eslint.with({
+
+  diagnostics_format = "[eslint] #{m}\n(#{c})",
+
+  -- only enable eslint if root has .eslintrc.js
+
+  condition = function(utils)
+
+  return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
+
+  end,
+
+  })
 }
 
 -- Avoiding LSP formatting conflicts
