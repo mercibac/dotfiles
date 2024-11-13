e53a7e7285ce4b2ef4155a4ff9562b38be91d9ce
mercibac <mercibacishoge@gmail.com>
2024-11-13 20:17:28 +0200

    add full-screen config on startup to wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -3,6 +3,12 @@ local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
+end)
 
 -- This is where you actually apply your config choices
 
