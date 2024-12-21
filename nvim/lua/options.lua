b3312f5bbe44cd4017cc1ce18e6176174025c01a
mercibac <mercibacishoge@gmail.com>
2024-12-21 08:33:53 +0200

    correct the place where to set options, not in init.lua but in options.lua

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,6 +1,39 @@
 require "nvchad.options"
 
--- add yours here!
+local vim = vim
+local enable_providers = {
+  "python3_provider",
+  "node_provider",
+  -- and so on
+}
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+for _, plugin in pairs(enable_providers) do
+  vim.g["loaded_" .. plugin] = nil
+  vim.cmd("runtime " .. plugin)
+end
+
+vim.g.mapleader = " "
+vim.o.shell = 'cmd.exe'
+vim.o.termguicolors = true
+vim.opt.fileencoding = "utf-8"
+vim.opt.relativenumber = true
+vim.opt.smartindent = true
+
+-- For asyncrun plugin
+vim.g.asyncrun_open = 6
+vim.g.asynctasks_template = '~/.config/nvim/task_template.ini'
+vim.g.asynctasks_term_pos = 'toggleterm2'
+vim.g.asynctasks_term_close = 1
+
+local lpath = "C:/Users/Merci_Bacman/.config/nvim/lua/custom/my_snippets"
+vim.g.luasnippets_path = lpath
+
+vim.cmd([[
+ augroup filetypedetect
+   autocmd BufNewFile,BufRead *.asm set filetype=nasm
+ augroup END
+]])
+
+-- add binaries installed by mason.nvim to path
+local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
+vim.env.PATH = vim.env.PATH .. (is_windows and ";" or ":") .. vim.fn.stdpath "data" .. "/mason/bin"
