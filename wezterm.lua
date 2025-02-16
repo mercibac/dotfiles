01e3e9a83fd8c9584a58cee3aba7824fbad16e0a
mercibac <mercibacishoge@gmail.com>
2025-02-16 17:51:02 +0200

    add shells labels

--- wezterm.lua
+++ wezterm.lua
@@ -10,6 +10,18 @@ wezterm.on("gui-startup", function(cmd)
     window:gui_window():maximize()
 end)
 
+config.set_environment_variables = {}
+
+-- if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+--   -- Use OSC 7 as per the above example
+--   config.set_environment_variables['prompt'] =
+--     '$E]7;file://localhost/$P$E\\$E[32m$T$E[0m $E[35m$P$E[36m$_$G$E[0m '
+--   -- use a more ls-like output format for dir
+--   config.set_environment_variables['DIRCMD'] = '/d'
+--   -- And inject clink into the command prompt
+--   config.default_prog =
+--     { 'cmd.exe', '/s', '/k', 'C:/Users/Merci_Bacman/scoop/apps/clink/1.6.14/clink_x64.exe', 'inject', '-q' }
+-- end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
@@ -24,6 +36,28 @@ config.ime_preedit_rendering = "Builtin"
 -- config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font 'FiraCode Nerd Font'
 config.default_prog = { 'nu' }
+config.launch_menu = {
+  {
+    label = 'nu',
+    args = { 'C:/Users/Merci_Bacman/scoop/shims/nu.exe'},
+  },
+  {
+    label = 'pwsh',
+    args = { 'C:/Program Files/PowerShell/7/pwsh.exe', '-nologo'},
+  },
+  {
+    label = 'cmd',
+    args = { 'cmd.exe','/k' },
+  },
+  {
+    label = 'bash',
+    args = { 'C:/Program Files/Git/bin/bash.exe' },
+  },
+  {
+    label = 'wsl',
+    args = { 'C:/Windows/System32/bash.exe'},
+  }
+}
 config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE"
 config.scrollback_lines = 184467440
