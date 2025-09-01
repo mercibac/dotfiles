08ad753052aca3a88e72004e532fa1c3241e4990
mercibac <mercibacishoge@gmail.com>
2025-09-01 08:37:19 +0200

    change python provider from python3 to python

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -2,7 +2,7 @@ require "nvchad.options"
 
 local vim = vim
 local enable_providers = {
-  "python3_provider",
+  "python_provider",
   "node_provider",
   -- and so on
 }
@@ -13,7 +13,7 @@ for _, plugin in pairs(enable_providers) do
 end
 
 vim.g.mapleader = " "
-vim.o.shell = 'cmd.exe'
+vim.o.shell = "cmd.exe"
 vim.o.termguicolors = true
 vim.opt.fileencoding = "utf-8"
 vim.opt.relativenumber = true
@@ -21,18 +21,18 @@ vim.opt.smartindent = true
 
 -- For asyncrun plugin
 vim.g.asyncrun_open = 6
-vim.g.asynctasks_template = '~/.config/nvim/task_template.ini'
-vim.g.asynctasks_term_pos = 'toggleterm2'
+vim.g.asynctasks_template = "~/.config/nvim/task_template.ini"
+vim.g.asynctasks_term_pos = "toggleterm2"
 vim.g.asynctasks_term_close = 1
 
 local lpath = "C:/Users/Merci_Bacman/.config/nvim/lua/custom/my_snippets"
 vim.g.luasnippets_path = lpath
 
-vim.cmd([[
+vim.cmd [[
  augroup filetypedetect
    autocmd BufNewFile,BufRead *.asm set filetype=nasm
  augroup END
-]])
+]]
 
 -- add binaries installed by mason.nvim to path
 local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
