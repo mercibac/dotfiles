8a81e0dc2706f4a1f88a3af0c28989d32b31e833
mercibac <mercibacishoge@gmail.com>
2025-09-01 08:38:16 +0200

    update the function format on save

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -1,15 +1,24 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
-
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+    -- Conform will run multiple formatters sequentially
+    -- You can customize     python = function(bufnr)
+    python = function(bufnr)
+      if require("conform").get_formatter_info("ruff_format", bufnr).available then
+        return { "ruff_format" }
+      else
+        return { "isort", "black" }
+      end
+    end,
+    javascript = { "prettierd", "prettier", stop_after_first = true },
+  }
 }
 
 require("conform").setup(options)
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
