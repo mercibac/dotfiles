9fdb6818ca89af2cbdda7394284cd2746a910a40
mercibac <mercibacishoge@gmail.com>
2025-09-07 00:34:46 +0200

    add logger plugin as a dependency of go-to-preview plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -420,6 +420,7 @@ local plugins = {
 
   {
     "rmagatti/goto-preview",
+    dependencies = { "rmagatti/logger.nvim" },
     event = "BufEnter",
     config = true, -- necessary as per https://github.com/rmagatti/goto-preview/issues/88
   },
