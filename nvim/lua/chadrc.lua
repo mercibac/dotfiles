86fd53848d2cfb69341f35d4ecd7516a7ab6c86c
mercibac <mercibacishoge@gmail.com>
2025-10-03 05:06:54 +0200

    fix the mappings module looping error (improve the previous fix)

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -1,14 +1,11 @@
 local M = {}
 
- M.plugins = 'plugins'
+M.plugins = "plugins"
 
- M.base46 = {
-  theme_toggle = { 'catppuccin', 'tokyonight', 'onedark', 'kanagawa' },
-  theme = 'onedark', -- default theme
+M.base46 = {
+  theme_toggle = { "catppuccin", "tokyonight", "onedark", "kanagawa" },
+  theme = "onedark", -- default theme
   transparency = true,
-
-  }
-
- M.mappings = 'mappings'
+}
 
 return M
