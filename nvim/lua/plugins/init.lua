bf5f3eff8291129d3ec7ea8da1f085bb6e70ba9c
mercibac <mercibacishoge@gmail.com>
2025-11-29 20:43:11 +0200

    add nvim-dev-container plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -70,7 +70,41 @@ local plugins = {
       },
     },
   },
-
+  {
+    "https://codeberg.org/esensar/nvim-dev-container",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    cmd = { "DevcontainerAttach", "DevcontainerStart", "DevcontainerStop", "DevcontainerExec" },
+    config = function()
+      require("devcontainer").setup {
+        autocommands = {
+          clean = true,
+          init = true,
+          update = true,
+        },
+        attach_mounts = {
+          neovim_config = {
+            -- enables mounting local config to /root/.config/nvim in container
+            enabled = true,
+            -- makes mount readonly in container
+            options = { "readonly" },
+          },
+          neovim_data = {
+            -- enables mounting local data to /root/.local/share/nvim in container
+            enabled = true,
+            -- no options by default
+            options = {},
+          },
+          -- Only useful if using neovim 0.8.0+
+          neovim_state = {
+            -- enables mounting local state to /root/.local/state/nvim in container
+            enabled = true,
+            -- no options by default
+            options = {},
+          },
+        },
+      }
+    end,
+  },
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "jvgrootveld/telescope-zoxide", "nvim-lua/popup.nvim" },
